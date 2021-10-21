57e471ed53af28b97608665c1ad0ea8b42cb1558
rkreber <rkreber9@gmail.com>
2021-10-21 17:00:32 +0000

    fixed test

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>The Dream Team's Website</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>WELCOME TO YOUR WILDEST DREAMS</h1>"));
         return done();
       });
 
